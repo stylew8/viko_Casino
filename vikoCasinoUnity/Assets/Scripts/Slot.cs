a15cda8657b271b35751c9f6cbb9eb2734c78b25
maxjke <m.jako030@gmail.com>
2023-12-11 22:56:47 +0200

    slot spin ne iki galo

---
+++ vikoCasinoUnity/Assets/Scripts/Slot.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SlotValue
+{
+    Crown,
+    Diamond,
+    Seven,
+    Cherry,
+    Bar
+}
+public class Slot : MonoBehaviour
+{
+
+    private int randomValue;
+    [HideInInspector]public float timeInterval;
+    [HideInInspector] public SlotValue randItem;
+    private float speed;
+    public SlotValue stoppedSlot;
+    private SlotMachine sm;
+
+    private void Start()
+    {
+        sm = gameObject.GetComponentInParent<SlotMachine>();
+    }
+    public IEnumerator Spin()
+    {
+        timeInterval = sm.timeInterval;
+        randomValue = Random.Range(0, 90);
+        speed = 30f + randomValue;
+        while(speed >= 10f)//for (int i = 0; i < 30+randomValue; i++)
+        {
+            speed = speed / 1.01f;
+            transform.Translate(Vector2.up * Time.deltaTime * -speed);
+            if (transform.localPosition.y <= -2.5f)
+            {
+                transform.localPosition = new Vector2(transform.localPosition.x, 2.17f);
+            }
+
+            yield return new WaitForSeconds(timeInterval);
+        }
+        StartCoroutine("EndSpin");
+        yield return null;
+    }
+    public IEnumerator CheatSpin()
+    {
+        timeInterval = sm.timeInterval;
+        randomValue = Random.Range(0, 90);
+        speed = 30f + randomValue;
+        while (speed >= 15f)
+        {
+            speed = speed / 1.01f;
+            transform.Translate(Vector2.up * Time.deltaTime * -speed);
+            if (transform.localPosition.y <= -2.5f)
+            {
+                transform.localPosition = new Vector2(transform.localPosition.x, 2.17f);
+            }
+
+            yield return new WaitForSeconds(timeInterval);
+        }
+        while (speed >= 2f)
+        {
+            if (randItem == SlotValue.Crown)
+            {
+                transform.localPosition = Vector2.MoveTowards(transform.localPosition, new Vector2(transform.localPosition.x, -2.5f), speed * Time.deltaTime);
+                if (new Vector2(transform.localPosition.x, transform.localPosition.y) == new Vector2(transform.localPosition.x, -2.5f))
+                {
+                    speed = 0;
+                }
+            }
+            else if (randItem == SlotValue.Diamond)
+            {
+                transform.localPosition = Vector2.MoveTowards(transform.localPosition, new Vector2(transform.localPosition.x, -1.55f), speed * Time.deltaTime);
+                if (new Vector2(transform.localPosition.x, transform.localPosition.y) == new Vector2(transform.localPosition.x, -1.55f))
+                {
+                    speed = 0;
+                }
+            }
+            else if (randItem == SlotValue.Seven)
+            {
+                transform.localPosition = Vector2.MoveTowards(transform.localPosition, new Vector2(transform.localPosition.x, -0.6f), speed * Time.deltaTime);
+                if (new Vector2(transform.localPosition.x, transform.localPosition.y) == new Vector2(transform.localPosition.x, -0.6f))
+                {
+                    speed = 0;
+                }
+            }
+            else if (randItem == SlotValue.Cherry)
+            {
+                transform.localPosition = Vector2.MoveTowards(transform.localPosition, new Vector2(transform.localPosition.x, 0.4f), speed * Time.deltaTime);
+                if (new Vector2(transform.localPosition.x, transform.localPosition.y) == new Vector2(transform.localPosition.x, 0.4f))
+                {
+                    speed = 0;
+                }
+            }
+            else if (randItem == SlotValue.Bar)
+            {
+                transform.localPosition = Vector2.MoveTowards(transform.localPosition, new Vector2(transform.localPosition.x, 1.35f), speed * Time.deltaTime);
+                if (new Vector2(transform.localPosition.x, transform.localPosition.y) == new Vector2(transform.localPosition.x, 1.35f))
+                {
+                    speed = 0;
+                }
+            }
+            speed = speed / 1.01f;
+            yield return new WaitForSeconds(timeInterval);
+        }
+        speed = 0;
+        CheckResults();
+        yield return null;
+    }
+    private IEnumerator EndSpin()
+    {
+        while (speed >= 2f)
+        {
+            if (transform.localPosition.y < -1.55f)
+            {
+                transform.localPosition = Vector2.MoveTowards(transform.localPosition, new Vector2(transform.localPosition.x, -2.5f), speed * Time.deltaTime);
+                if (new Vector2(transform.localPosition.x, transform.localPosition.y) == new Vector2(transform.localPosition.x, -2.5f))
+                {
+                    speed = 0;
+                }
+            }
+            else if (transform.localPosition.y < -0.6f)
+            {
+                transform.localPosition = Vector2.MoveTowards(transform.localPosition, new Vector2(transform.localPosition.x, -1.55f), speed * Time.deltaTime);
+                if (new Vector2(transform.localPosition.x, transform.localPosition.y) == new Vector2(transform.localPosition.x, -1.55f))
+                {
+                    speed = 0;
+                }
+            }
+            else if (transform.localPosition.y < 0.4f)
+            {
+                transform.localPosition = Vector2.MoveTowards(transform.localPosition, new Vector2(transform.localPosition.x, -0.6f), speed * Time.deltaTime);
+                if (new Vector2(transform.localPosition.x, transform.localPosition.y) == new Vector2(transform.localPosition.x, -0.6f))
+                {
+                    speed = 0;
+                }
+            }
+            else if (transform.localPosition.y < 1.35f)
+            {
+                transform.localPosition = Vector2.MoveTowards(transform.localPosition, new Vector2(transform.localPosition.x, 0.4f), speed * Time.deltaTime);
+                if (new Vector2(transform.localPosition.x, transform.localPosition.y) == new Vector2(transform.localPosition.x, 0.4f))
+                {
+                    speed = 0;
+                }
+            }
+            else if (transform.localPosition.y < 2.17f)
+            {
+                transform.localPosition = Vector2.MoveTowards(transform.localPosition, new Vector2(transform.localPosition.x, 1.35f), speed * Time.deltaTime);
+                if (new Vector2(transform.localPosition.x, transform.localPosition.y) == new Vector2(transform.localPosition.x, 1.35f))
+                {
+                    speed = 0;
+                }
+            }
+            speed = speed / 1.01f;
+            yield return new WaitForSeconds(timeInterval);
+        }
+        speed = 0;
+        CheckResults();
+        yield return null;
+    }
+    private void CheckResults()
+    {
+        if (transform.localPosition.y == -2.5f)
+        {
+            stoppedSlot = SlotValue.Crown;
+        }
+        else if (transform.localPosition.y == -1.55f)
+        {
+            stoppedSlot = SlotValue.Diamond;
+        }
+        else if (transform.localPosition.y == -0.6f)
+        {
+            stoppedSlot = SlotValue.Seven;
+        }
+        else if (transform.localPosition.y == 0.4f)
+        {
+            stoppedSlot = SlotValue.Cherry;
+        }
+        else if (transform.localPosition.y == 1.35f)
+        {
+            stoppedSlot = SlotValue.Bar;
+        }
+
+        sm.WaitResults();
+    }
+}
---
+++ vikoCasinoUnity/Assets/Scripts/SlotMachine.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlotMachine : MonoBehaviour
+{
+    public int money;
+    public Text moneyText;
+    public int price;
+    public Slot[] slots;
+    public Combinations[] combinations;
+    public float timeInterval = 0.025f;
+    private int stoppedSlots = 3;
+    private bool isSpin = false;
+    public bool isAuto;
+    public Animator btnAnim;
+
+    private void Start()
+    {
+        btnAnim = GameObject.FindGameObjectWithTag("AutoButton").GetComponent<Animator>();
+    }
+
+    public void Spin()
+    {
+        if (!isSpin && money - price >= 0)
+        {
+            ChangeMoney(-price);
+            isSpin = true;
+            foreach (Slot i in slots)
+            {
+                i.StartCoroutine("Spin");
+            }
+        }
+    }
+
+    public void WaitResults()
+    {
+        stoppedSlots -= 1;
+        if(stoppedSlots <= 0)
+        {
+            stoppedSlots = 3;
+            CheckResults();
+        }
+    }
+
+    public void CheckResults()
+    {
+        isSpin = false;
+        foreach (Combinations i in combinations)
+        {
+            Debug.Log(slots[0].gameObject.GetComponent<Slot>().stoppedSlot.ToString());
+            Debug.Log(i.FirstValue.ToString());
+            if (slots[0].gameObject.GetComponent<Slot>().stoppedSlot.ToString() == i.FirstValue.ToString()
+                && slots[1].gameObject.GetComponent<Slot>().stoppedSlot.ToString() == i.SecondValue.ToString()
+                && slots[2].gameObject.GetComponent<Slot>().stoppedSlot.ToString() == i.ThirdValue.ToString())
+            {
+                ChangeMoney(i.prize);
+            }
+        }
+        if (isAuto)
+        {
+            Invoke("Spin", 0.4f);
+        }
+    }
+    public void OnlyVictory()
+    {
+        if (!isSpin && money - price >= 0)
+        {
+            ChangeMoney(-price);
+            isSpin = true;
+            SlotValue randItem = (SlotValue)Random.Range(0, 5);
+            foreach (Slot i in slots)
+            {
+                i.randItem = randItem;
+                i.StartCoroutine("CheatSpin");
+            }
+        }
+    }
+    private void ChangeMoney(int count)
+    {
+        money += count;
+        moneyText.text = money.ToString();
+    }
+    public void SetAuto()
+    {
+        if (!isAuto)
+        {
+            timeInterval = timeInterval / 10;
+            isAuto = true;
+            btnAnim.SetBool("isAuto", true);
+            Spin();
+        }
+        else
+        {
+            timeInterval = timeInterval * 10;
+            isAuto = false;
+            btnAnim.SetBool("isAuto", false);
+        }
+    }
+}
+
+[System.Serializable]
+public class Combinations
+{
+    public enum SlotValue
+    {
+        Crown,
+        Diamond,
+        Seven,
+        Cherry,
+        Bar
+    }
+
+    public SlotValue FirstValue;
+    public SlotValue SecondValue;
+    public SlotValue ThirdValue;
+    public int prize;
+}
